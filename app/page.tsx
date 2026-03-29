dadaeee638ef9fa58369e9d2149bfe3d616a2266
Krsytian <kevinexon@gmail.com>
2026-03-29 22:26:29 +0200

    all done

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,110 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="max-w-480 m-auto min-w-97.5">
+      
+      <section className="top-bunner">
+        <div className="w-full relative">
+          <img src="/photos/kwiaty.jpeg" alt="Zdjęcie wiosennych kwiatów" className="w-full max-h-175" />
+          <div className="absolute top-17/48 left-1/8 w-6/8 2xl:top-19/48 2xl:left-1/4 2xl:w-1/2">
+            <img src="/photos/lorena-krystian.svg" alt="Lorena i Krystian" className="w-full" style={{ paddingLeft: '10%' }} />
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </section>
+
+      <section className="invitation-content mt-10 md:mt-12 lg:mt-14 xl:mt-16 2xl:mt-20">
+        <div className="w-full">
+          <div className="w-1/8 min-w-24 mx-auto">
+            <img src="/photos/golobki.svg" alt="Grafika z dwoma gołąbkami z obrączkami"/>
+          </div>
+
+          <div className="text-center font-semibold text-lg lg:text-2xl xl:text-3xl mt-6 md:mt-8 lg:mt-10 xl:mt-12 2xl:mt-14">
+            <p>SERDZECZNIE ZAPRASZAMY</p>
+            <p>NA NASZ ŚLUB I WESELE</p>
+          </div>
+
+
+          <div className="text-center px-18 md:text-lg xl:text-xl mt-12 lg:mt-16 2xl:mt-20">
+            <div className="hidden md:block">
+              <p>CEREMONIA ZAŚLUBIN ODBĘDZIE SIĘ W DNIU <span className="text-xl xl:text-2xl">02.05.2026</span></p>
+              <p>O GODZINIE <span className="text-xl xl:text-2xl">12:30</span> W PAŁACU ŚLUBÓW</p>
+              <p className="text-xs lg:text-base mt-2"><span className="material-icons" style={{ fontSize: '1rem', position: 'relative', top: '2px' }}>location_on</span> PLAC ZAMKOWY 6, 00-281 WARSZAWA</p>
+            </div>
+
+            <div className="md:hidden">
+              <p>CEREMONIA ZAŚLUBIN ODBĘDZIE SIĘ</p>
+              <p>W DNIU <span className="text-xl xl:text-2xl">02.05.2026</span></p>
+              <p>O GODZINIE <span className="text-xl xl:text-2xl">12:30</span> W PAŁACU ŚLUBÓW</p>
+              <p className="text-xs lg:text-base mt-2"><span className="material-icons" style={{ fontSize: '1rem', position: 'relative', top: '2px' }}>location_on</span> PLAC ZAMKOWY 6, 00-281 WARSZAWA</p>
+            </div>
+          </div>
+
+          <div className="text-center px-18 md:text-lg xl:text-xl mt-12 lg:mt-16 2xl:mt-20">
+            <div className="hidden md:block">
+              <p>NASTĘPNIE ZAPRASZAMY NA WESELE</p>
+              <p>W PAŁACU KRZYKOSY</p>
+              <p className="text-xs lg:text-base mt-2"><span className="material-icons" style={{ fontSize: '1rem', position: 'relative', top: '2px' }}>location_on</span> KRZYKOSY 48, 09-454 KRZYKOSY</p>
+            </div>
+
+            <div className="md:hidden">
+              <p>NASTĘPNIE ZAPRASZAMY</p>
+              <p>NA WESELE</p>
+              <p>W PAŁACU KRZYKOSY</p>
+              <p className="text-xs lg:text-base mt-2"><span className="material-icons" style={{ fontSize: '1rem', position: 'relative', top: '2px' }}>location_on</span> KRZYKOSY 48, 09-454 KRZYKOSY</p>
+            </div>
+          </div>
+
+          <div className="text-center px-18 md:text-lg xl:text-xl mt-6 lg:mt-10 2xl:mt-14">
+            <div className="hidden md:block">
+              <p>NA MALOWNICZY TEREN PAŁACU ZAPRASZAMY OD GODZINY <span className="text-xl xl:text-2xl">15:30</span>,</p>
+              <p>CZĘŚĆ OFICJALNA ROZPOCZNIE SIĘ O <span className="text-xl xl:text-2xl">16:00</span></p>
+            </div>
+
+            <div className="md:hidden">              
+              <p>NA MALOWNICZY TEREN PAŁACU</p>
+              <p>ZAPRASZAMY OD GODZINY <span className="text-xl xl:text-2xl">15:30</span>,</p>
+              <p>CZĘŚĆ OFICJALNA</p>
+              <p>ROZPOCZNIE SIĘ O <span className="text-xl xl:text-2xl">16:00</span></p>
+            </div>
+          </div>
+
+          <div className="text-center px-18 md:text-lg xl:text-xl mt-6 lg:mt-10 2xl:mt-14">
+            <div className="hidden md:block">
+              <p>NA MIEJSCU BĘDZIE ZAPEWNIONY BUFET, ALKOHOL ORAZ OGNISKO.</p>
+              <p>JEMY, PIJEMY I TAŃCZYMY DO RANA!</p>
+            </div>
+
+            <div className="md:hidden">
+              <p>NA MIEJSCU BĘDZIE ZAPEWNIONY</p>
+              <p> BUFET, ALKOHOL ORAZ OGNISKO.</p>
+              <p>JEMY, PIJEMY I TAŃCZYMY DO RANA!</p>
+            </div>
+          </div>
+
+          <div className="w-1/12 min-w-18 mx-auto mt-6 md:mt-8 lg:mt-10 xl:mt-12 2xl:mt-14">
+            <img src="/photos/wstega2.svg" alt="Wstęga ozdobna"/>
+          </div>
+
+          <div className="w-3/4 mx-auto">
+            <img src="/photos/dresscode.svg" alt="Grafika opisująca dresscode, oraz trzy zdjęcia pary młodej z budki"/>
+          </div>
+
+          <div className="w-1/8 min-w-24 mx-auto mt-6 md:mt-8 lg:mt-10 xl:mt-12 2xl:mt-14">
+            <img src="/photos/wstega1.svg" alt="Wstęga ozdobna"/>
+          </div>
+
+          <div className="w-full relative mt-6 md:mt-8 lg:mt-10 xl:mt-12 2xl:mt-14">
+            <img src="/photos/palac.png" alt="Zdjęcie pałacu w Krzykosach" className="w-full"/>
+
+            <img src="/photos/zyrandol.svg" 
+                 alt="Grafika żyrandolu bankietowego" 
+                 className="absolute top-10 w-1/10 left-9/20"/>
+
+            <img src="/photos/widzimy-sie.svg" 
+                 alt="Grafika żyrandolu bankietowego" 
+                 className="absolute bottom-10 w-3/5 left-1/5"/>
+          </div>
         </div>
-      </main>
-    </div>
+      </section>
+    </main>
   );
 }
