78de3eefc14e047de72e82ee7a20540c2f8293cc
Krsytian <kevinexon@gmail.com>
2026-03-31 18:16:33 +0200

    base url fixing

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  basePath: isProduction ? "/strona-slubna" : "",
+  basePath: "/strona-slubna",
   output: "export",
   distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
