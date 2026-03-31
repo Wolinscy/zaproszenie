97afc7833a2a9ae69cfcb985a8d3fb32c92695de
Krsytian <kevinexon@gmail.com>
2026-03-31 17:40:47 +0200

    next.config changed

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  basePath: isProduction ? "/strona-slubna" : "",
   output: "export",
   distDir: "dist",
 };
